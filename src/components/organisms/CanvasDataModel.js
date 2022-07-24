23c3e09535febdeca0cfcccba9183090acd04d84
Nhan Nguyen <Nhan.Nguyen@questanalytics.com>
2022-07-24 08:36:14 -0500

    on save upload to pinata function

--- src/components/organisms/CanvasDataModel.js
+++ src/components/organisms/CanvasDataModel.js
@@ -20,7 +20,7 @@ export const AnnotationsContext = React.createContext({
 
 export default function CanvasDataModel() {
     const [notes, setNotes] = React.useState({0: {text: "hello", position: {x: 0, y: 0}}, 1: {text: "hello", position: {x: 0, y: 200}}, 2: {text: "hello", position: {x: 0, y: 400}}});
-    const [clips, setClips] = React.useState({10: {fileId: "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3", span: {start: 0, end: 100}, position: {x: 0, y: 0}}});
+    const [clips, setClips] = React.useState({});
     const [files, setFiles] = React.useState({});
     const [annotations, setAnnotations] = React.useState({});
 
@@ -41,27 +41,35 @@ export default function CanvasDataModel() {
         annotations
     }
 
-    // function saveContext() {
-    //     var axios = require('axios');
-    //     var Readable = require('stream').Readable;
-    //     var data = new Readable;
-    //     data.push(JSON.stringify(notes));
-    //     data.append('file', data);
-    //     data.append('pinataOptions', '{"cidVersion": 1"}');
-    //     data.append('pinataMetadata', '{"name": "MyFile", "keyvalues": {"company": "Pinata"}}');
-
-    //     var config = {
-    //         method: 'post',
-    //         url: 'https://api.pinata.cloud/pinning/pinJSONToIPFS', 
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI4MmI2MjJmNi1lMDAyLTQzNzktYTZiMi0zYTJmMTZmZTc0ODIiLCJlbWFpbCI6ImFmcmV2ZXJ0QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiJkN2I3MWExODg5NDU2NDE0NmY5NiIsInNjb3BlZEtleVNlY3JldCI6ImQ2ZWJlMDBjNTQwMjViZmZkMTlhOGUxYTQyZGZjY2FjZjk0MmZjZjAxNGJiMDU4MmUxMjY2Mjk5MzFmNDJiMTgiLCJpYXQiOjE2NTg1OTczODN9.id8CavzoeQbXoV8HZApAsrrw1rJLUO2SKJ-Ki9C7KZs'
-    //         },
-    //         data: notesJson
-    //     };
-
-    //     axios(config);
-    // }
+    function onSave() {
+        var axios = require('axios');
+        var data = JSON.stringify({
+            "pinataOptions": {"cidVersion": 1
+            },
+            "pinataMetadata": {
+                "name": "test_upload",
+                "keyvalues": {
+                    "type": "test"
+                }
+            },
+            "pinataContent": {
+                "notes": notes
+            }
+        });
+        console.log(data);
+    
+        var config = {
+            method: 'post',
+            url: 'https://api.pinata.cloud/pinning/pinJSONToIPFS', 
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI4MmI2MjJmNi1lMDAyLTQzNzktYTZiMi0zYTJmMTZmZTc0ODIiLCJlbWFpbCI6ImFmcmV2ZXJ0QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiJkN2I3MWExODg5NDU2NDE0NmY5NiIsInNjb3BlZEtleVNlY3JldCI6ImQ2ZWJlMDBjNTQwMjViZmZkMTlhOGUxYTQyZGZjY2FjZjk0MmZjZjAxNGJiMDU4MmUxMjY2Mjk5MzFmNDJiMTgiLCJpYXQiOjE2NTg1OTczODN9.id8CavzoeQbXoV8HZApAsrrw1rJLUO2SKJ-Ki9C7KZs'
+            },
+            data: data
+        };
+    
+        console.log(axios(config));
+    }
 
     return (
         <NotesContext.Provider value={notesContext}>
